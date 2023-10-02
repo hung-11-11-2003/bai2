b4f8aadde513d5cd2d3cefa876f469cf98ae4d48
Trần Nguyễn Phi Hùng <phihungmap3@gmail.com>
2023-10-02 15:38:08 +0700

    Create SachOnlineController.cs

---
+++ SachOnline/Controllers/SachOnlineController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel;
+using SachOnline.Models;
+
+namespace SachOnline.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        private string connection;
+        private dbSachOnlineDataContext data;
+
+        private List<SACH> LaySachMoi(int count)
+        {
+            return data.SACHes.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        }
+        public SachOnlineController()
+        {
+            // Khởi tạo chuỗi kết nối
+            connection = "Data Source=DESKTOP-8DF9EK9\\SQLEXPRESS;Initial Catalog=SachOnline;Integrated Security=True";
+            data = new dbSachOnlineDataContext(connection);
+        }
+
+        // GET: SachOnline
+        public ActionResult Index()
+        {
+            var listSachMoi = LaySachMoi(6);
+            return View(listSachMoi);
+        }
+        public ActionResult ChuDePartial()
+        {
+            var listChuDe = from cd in data.CHUDEs select cd;
+            return PartialView(listChuDe);
+        }
+        public ActionResult NhaXuatBanPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult SachBanNhieuPartial()
+        {
+            var listNXB = from cd in data.NHAXUATBANs select cd;
+            return PartialView(listNXB);
+        }
+
+
+    }
+}
